ecd179f7dcb644aed943a912f7bccd5847ce75a4
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-01-09 15:49:27 +0100

    Made multiple blogs possible.

--- lfc_blog/urls.py
+++ lfc_blog/urls.py
@@ -2,8 +2,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns("lfc_blog.views",
-    url(r'tag/(?P<slug>[-\w]+)/(?P<tag>[^/]+)/$', "lfc_tagged_object_list", name="lfc_blog_tag_list"),
-    url(r'^archive/(?P<slug>[-\w]+)/(?P<month>\d{1,2})/(?P<year>\d{4})/$', "archive", name="lfc_archive"),
+    url(r'(?P<slug>.+)/tag/(?P<tag>[^/]+)$', "lfc_tagged_object_list", name="lfc_blog_tag_list"),
+    url(r'^(?P<slug>.+)/archive/(?P<month>\d{1,2})/(?P<year>\d{4})$', "archive", name="lfc_archive"),
 )
-
-    
--- lfc_blog/views.py
+++ lfc_blog/views.py
@@ -12,6 +12,9 @@ from django.utils.translation import ugettext_lazy as _
 from tagging.models import TaggedItem
 from tagging.utils import get_tag
 
+# lfc imports
+from lfc.utils import traverse_object
+
 # lfc_blog imports
 from lfc_blog.models import Blog
 from lfc_blog.models import BlogEntry
@@ -19,16 +22,12 @@ from lfc_blog.models import BlogEntry
 def archive(request, slug, month, year, template_name="lfc_blog/archive.html"):
     """Display blog entries for given month, year and language.
     """
-    entries = BlogEntry.objects.filter(
-        parent__slug=slug,
-        active=True,
-        language__in=(translation.get_language(), "0"),
-        creation_date__month=month,
-        creation_date__year=year,
-    )
+    blog = traverse_object(request, slug)
+
+    entries = blog.sub_objects.all()
 
     return render_to_response(template_name, RequestContext(request, {
-        "blog" : Blog.objects.get(slug=slug),
+        "blog" : blog,
         "month" : _(datetime.date(int(year), int(month), 1).strftime('%B')),
         "year" : year,
         "entries" : entries
@@ -44,13 +43,10 @@ def lfc_tagged_object_list(request, slug, tag, template_name="lfc_blog/tag.html"
     if tag_instance is None:
         raise Http404(_('No Tag found matching "%s".') % tag)
 
-    try:
-        blog = Blog.objects.get(slug=slug)
-    except Blog.DoesNotExist:
-        raise Http404()
-    else:
-        queryset = BlogEntry.objects.filter(parent=blog)
-        entries = TaggedItem.objects.get_by_model(queryset, tag_instance)
+    blog = traverse_object(request, slug)
+
+    queryset = BlogEntry.objects.filter(parent=blog)
+    entries = TaggedItem.objects.get_by_model(queryset, tag_instance)
 
     return render_to_response(template_name, RequestContext(request, {
         "slug"    : slug,
