6eb9599861115ace7f81c3f0353d48fc63daeb61
Kai Diefenbach <kai.diefenbach@iqpp.de>
2014-06-15 07:47:29 +0200

    Adds Django 1.6 support.

--- lfc_blog/urls.py
+++ lfc_blog/urls.py
@@ -1,5 +1,6 @@
 # django imports
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns
+from django.conf.urls import url
 
 urlpatterns = patterns("lfc_blog.views",
     url(r'^(?P<language>[-\w]{2})/(?P<slug>.+)/tag/(?P<tag>[^/]+)$', "lfc_tagged_object_list", name="lfc_blog_tag_list"),
